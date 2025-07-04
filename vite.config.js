0769df4908285e99b17cbad3ac06d34b1f0b9d9f
신성현 <froggy0m0a@gmail.com>
2025-07-04 19:14:41 +0900

    feat: PWA 적용을 위한 초기 설정
    - vite.config.js에 vite-plugin-pwa 플러그인 추가
    - manifest 정보 및 아이콘 설정
    - index.html에 PWA 관련 메타 태그 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate', // 서비스 워커 업데이트 방식을 자동으로 처리
+            devOptions: {
+                enabled: true, // 개발 환경에서도 PWA를 테스트할 수 있도록 활성화
+            },
+            manifest: {
+                name: 'Ringling', // 앱 이름
+                short_name: 'Ringling',
+                description: '출근길에 저장 콘텐츠를 푸시 알림해봐요!', // 앱 설명
+                icons: [
+                    {
+                        src: 'vite.svg',
+                        sizes: 'any',
+                        type: 'image/svg+xml'
+                    }
+                ]
+            },
+        })
+    ],
 })
